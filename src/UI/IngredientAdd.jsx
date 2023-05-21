3a033b4f0007d9b244894712635b266bfd14379d
3n1tan <118386299+3n1tan@users.noreply.github.com>
2023-05-21 19:41:17 +0300

    more changes

--- src/UI/IngredientAdd.jsx
+++ src/UI/IngredientAdd.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { styled } from 'styled-components'
 
-const IngredientAdd = ({quantityChange, nameChange, i}) => {
+const IngredientAdd = ({quantityAdd, nameAdd, i}) => {
   return (
     <Container>
         <div>
